abda1396e0d972e7648e79e4160f9a703f146d7a
Gourav Kumar <76958987+Luciferxgkp@users.noreply.github.com>
2023-08-13 15:29:04 +0530

    refactor: home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl">Lucifer.ai</h1>
 
-      <p className="text-foreground">An open source AI for the masses</p>
+      <p className="text-foreground">An open source AI platform</p>
       <Button>Get Started</Button>
     </div>
   );
